6757f9f4c90171e5b4b421de97d36b0d5187791b
Christos Dimoulas <chrdimo@seas.harvard.edu>
2017-09-05 20:07:57 -0400

    basic nsf lang setup

--- info.rkt
+++ info.rkt
@@ -5,5 +5,5 @@
 (define deps '("base" "scribble-lib" "at-exp-lib"))
 (define build-deps '("racket-doc" "scribble-doc"))
 
-(define pkg-desc "A Scribble document styl for NSF proposals")
+(define pkg-desc "A Scribble document class for NSF proposals")
 (define pkg-authors '(chrdimo))
--- nsf.rkt
+++ nsf.rkt
@@ -1,41 +1,74 @@
 #lang racket/base
 
-(require scribble/base scribble/decode
-         (except-in scribble/core paragraph)
+(require (except-in scribble/base author)
+         scribble/decode
+         scribble/core
          (rename-in scribble/doclang [#%module-begin -#%module-begin])
          scribble/private/defaults
-         scribble/html-properties scribble/latex-properties
-         setup/main-collects
-         setup/collects
+         racket/list
          (for-syntax racket/base
                      racket/syntax))
 
 (provide (all-from-out scribble/base)
          (except-out (all-from-out scribble/doclang)
                      -#%module-begin)
-         (rename-out [--#%module-begin #%module-begin]))
+         (rename-out [--#%module-begin #%module-begin])
+         author
+         turn-page+reset
+         chapter
+         titled-para)
 
-(define ((post-process [point "11pt"]) doc)
+
+(define ((post-process) doc)
   (add-defaults doc
-                (string-append
-                 "\\documentclass["
-                 point
-                 "]{article}\n"
-                 "\\usepackage[empty]{fullpage}\n"
-                 "\\pagestyle{plain}\n"
-                 "\\setlength{\footskip}{0.5in}\n"
+                (string->bytes/utf-8 #<<FORMAT
+\PassOptionsToPackage{usenames,dvipsnames}{color}
+\documentclass[11pt]{article}
+\usepackage[empty]{fullpage}
+\pagestyle{plain}
+\setlength{\footskip}{0.5in}
+\bibliographystyle{abbrvnat}
+FORMAT
+                                     )
                 (collection-file-path "style.tex" "scribble" "nsf")
                 null
-                #f)))
+                #f))
+
 
 (define-syntax (--#%module-begin stx)
   (syntax-case stx ()
-    [(_ journal ?e ...)
+    [(_ ?e ...)
      (with-syntax ([doc (format-id stx "doc")])
        (quasisyntax/loc stx
-         (-#%module-begin doc (post-process '#,(syntax->datum #'journal)) () ?e ...)))]))
+         (-#%module-begin doc (post-process) () ?e ...)))]))
 
-;; Reader configuration for #lang
+
+
+(define (author names department university)
+  (make-paragraph 
+   (make-style 'author null)
+   (let ([nl (make-element (make-style #f '(exact-chars)) "\\\\")]
+         [names
+          (case (length names)
+            [(2) (string-append (car names) " and " (cadr names))]
+            [else (add-between names " ")])])
+     (list names nl department nl university))))
+
+
+(define (turn-page+reset)
+ (make-element (make-style #f '(exact-chars)) "\\nsfturnpageandreset"))
+
+(define (chapter proposal-title chapter-title)
+ (make-multiarg-element "nsfchapter"
+                        (list (decode-content (list proposal-title))
+                              (decode-content (list chapter-title)))))     
+  
+(define (titled-para title . content)
+  (make-multiarg-element "nsftitledpara"
+                        (list (decode-content (list title))
+                              (decode-content content))))
+
+
 (module reader scribble/base/reader
-  scribble/nsf
-  #:wrapper1 (lambda (t) (t)))
+scribble/nsf
+#:wrapper1 (lambda (t) (t)))
